f0933b77eac01bd670614c34b67b208e1afb77ea
Pavel Kolomeitsev <pavelkolomeitsev1981@gmail.com>
2021-11-07 15:22:36 +0200

    implement progress bar

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -1,9 +1,17 @@
+import { LoadingBar } from "../utils";
+
 export class PreloadScene extends Phaser.Scene {
     constructor() {
         super({key: "preload-scene"});
     }
 
     protected preload() {
+        this.add.sprite(0, 0, "bg").setOrigin(0, 0);
+        const loadingBar: LoadingBar = new LoadingBar(this);
+        this.preloadAssets();
+    }
+
+    protected preloadAssets() {
         this.load.atlas("dragon", "assets/images/dragon.png", "assets/images/dragon.json");
         this.load.atlas("enemy", "assets/images/enemy.png", "assets/images/enemy.json");
         this.load.atlas("boom", "assets/images/boom.png", "assets/images/boom.json");
--- src/utils.ts
+++ src/utils.ts
@@ -35,3 +35,51 @@ export const FlyingType = {
 export const DRAGON_SPEED: number = 500;
 export const ENEMY_SPEED: number = 250;
 export const ENEMIES_AMOUNT: number = 10;
+
+export class LoadingBar {
+    private scene: Phaser.Scene;
+    private style: any;
+    private progressBox: Phaser.GameObjects.Graphics;
+    private progressBar: Phaser.GameObjects.Graphics;
+
+    constructor(scene: Phaser.Scene) {
+        this.scene = scene;
+        this.style = {
+            boxColor: 0xD3D3D3,
+            barColor: 0xFFF8DC,
+            x: BrowserResolution.WIDTH / 2 - 450,
+            y: BrowserResolution.HEIGHT / 2 + 250,
+            width: 900,
+            height: 25
+        };
+        this.progressBox = new Phaser.GameObjects.Graphics(this.scene);
+        this.progressBox = this.scene.add.graphics();
+        this.progressBar = this.scene.add.graphics();
+
+        this.showProgressBox();
+        this.setEvents();
+    }
+
+    setEvents() {
+        this.scene.load.on("progress", this.showProgressBar, this);
+        this.scene.load.on("complete", this.onLoadComplete, this);
+    }
+
+    showProgressBox() {
+        this.progressBox
+            .fillStyle(this.style.boxColor) // grey color
+            .fillRect(this.style.x, this.style.y, this.style.width, this.style.height);
+    }
+
+    showProgressBar(value: number) {
+        this.progressBar
+            .clear()
+            .fillStyle(this.style.barColor) // yellow color
+            .fillRect(this.style.x, this.style.y, this.style.width * value, this.style.height);
+    }
+
+    onLoadComplete() {
+        this.progressBox.destroy();
+        this.progressBar.destroy();
+    }
+}
