5281eb6a8bc02c7c8f5507199d89a2039a09bee9
Pavel Kolomeitsev <pavelkolomeitsev1981@gmail.com>
2021-12-18 21:44:20 +0200

    add .js to webpack prod file

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         path: path.resolve(__dirname, "dist")
     },
     resolve: {
-        extensions: [".ts"]
+        extensions: [".ts", ".js"]
     },
     module: {
         rules: [
